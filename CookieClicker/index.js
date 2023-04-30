cc755954a9053f255a825e5f1423d99329f048af
R ansh joseph <ranshjosrph@gmail.com>
2023-04-30 06:46:51 +0530

    clicking is working

--- CookieClicker/index.js
+++ CookieClicker/index.js
@@ -1,12 +1,50 @@
 let dog = document.getElementById("dog")
+let imags = [document.getElementById("1"),
+document.getElementById("2"),
+document.getElementById("3"),
+document.getElementById("4")]
+let count = 0
+let prev = 0
 
-function Animation(){
-    dog.setAttribute("src","./assert/2.png")
-    dog.setAttribute("src","./assert/3.png")
-    dog.setAttribute("src","./assert/4.png")
-    dog.setAttribute("src","./assert/1.png")
+imags[1].style.display = "none"
+imags[2].style.display = "none"
+imags[3].style.display = "none"
+function Animation() {
+    dog.setAttribute("src", "./assert/2.png")
+    dog.setAttribute("src", "./assert/3.png")
+    dog.setAttribute("src", "./assert/4.png")
+    dog.setAttribute("src", "./assert/1.png")
 }
 
-dog.addEventListener('click',()=>{
-    console.log("click")
+imags[0].addEventListener('click', () => {
+    prev = count
+    count++;
+    // prev
+    imags[prev%4].style.display= "none"
+    imags[count%4].style.display = "block"
+
+})
+imags[1].addEventListener('click', () => {
+    prev = count
+    count++;
+    // prev
+    imags[prev%4].style.display= "none"
+    imags[count%4].style.display = "block"
+
+})
+imags[2].addEventListener('click', () => {
+    prev = count
+    count++;
+    // prev
+    imags[prev%4].style.display= "none"
+    imags[count%4].style.display = "block"
+
+})
+imags[3].addEventListener('click', () => {
+    prev = count
+    count++;
+    // prev
+    imags[prev%4].style.display= "none"
+    imags[count%4].style.display = "block"
+
 })
